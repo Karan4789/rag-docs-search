e907d8164941d003af7d8dad91a424cd80856445
Karan4789 <karan2301.m@gmail.com>
2026-07-06 13:27:01 +0530

    pymudpdf4llm testing

--- test.py
+++ test.py
@@ -1,17 +1,13 @@
-from src.ingestion.loaders.docling_loader import load_docling
+import pymupdf4llm
 
-docs = load_docling("data/Book.pdf")
+pages = pymupdf4llm.to_markdown(
+    "data/Book.pdf",
+    page_chunks=True
+)
 
-print("Documents:", len(docs))
-
-print()
-
-print("Metadata")
-
-print(docs[10].metadata)
+print(type(pages))
+print(len(pages))
 
 print()
 
-print("Content")
-
-print(docs[0].page_content[:1500])
+print(pages[0].keys())
